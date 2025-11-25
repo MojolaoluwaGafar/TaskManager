ad68bc396bfed6c41a19963a923bacb6b807ff21
Mojolaoluwa Gafar <Mojolaoluwa1212@gmail.com>
2025-11-25 15:32:51 +0100

    type import issue fix

--- src/Services/Api.ts
+++ src/Services/Api.ts
@@ -22,20 +22,20 @@
 //   });
 import axios from "axios";
 
-const taskAPI = axios.create({
-  baseURL: "https://taskmanagerserver-zw50.onrender.com/api/tasks",
-});
+// const taskAPI = axios.create({
+//   baseURL: "https://taskmanagerserver-zw50.onrender.com/api/tasks",
+// });
 
-taskAPI.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    if (!config.headers) config.headers = {};
-    config.headers["Authorization"] = `Bearer ${token}`;
-  }
-  return config;
-});
+// taskAPI.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     if (!config.headers) config.headers = {};
+//     config.headers["Authorization"] = `Bearer ${token}`;
+//   }
+//   return config;
+// });
 
-export default taskAPI;
+// export default taskAPI;
 
 const API = axios.create({
   baseURL: "https://taskmanagerserver-zw50.onrender.com/api/users",
