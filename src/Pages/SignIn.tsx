e894d4d146b1c4e4067fa1c258c43e26c2509264
Mojolaoluwa Gafar <Mojolaoluwa1212@gmail.com>
2025-11-25 03:58:59 +0100

    integration

--- src/Pages/SignIn.tsx
+++ src/Pages/SignIn.tsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router";
 export default function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const [error] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent) => {
--- src/Types/task.ts
+++ src/Types/task.ts
@@ -1,8 +1,17 @@
-export interface Task  {
-  id: string;
+// export interface Task  {
+//   id: string;
+//   title: string;
+//   completed: boolean;
+//   created_at?: string;
+// }
+
+
+export interface Task {
+  id: number;
   title: string;
   completed: boolean;
+  description?: string;
+  priority?: "low" | "normal" | "high";
+  due_date?: string;
   created_at?: string;
 }
-
-
